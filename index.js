dec367bf22366b1c610c49032f8b33090e6195ff
Andy Nelson <54290062+ANelson82@users.noreply.github.com>
2025-07-22 21:10:15 -0500

    5

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+
+let person = {
+  name: "Andy",
+  age: 30,
+}; //object literal
+
+// Dot Notation
+person.name = "John";
+
+// Bracket Notation
+
+person["name"] = "Alex";
+
+console.log(person.name);
